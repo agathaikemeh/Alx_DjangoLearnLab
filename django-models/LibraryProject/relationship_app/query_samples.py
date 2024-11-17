685af99ff555a6382806477c175c908cb4083f66
Ikemeh Agatha <ikemehagatha@gmail.com>
2024-11-17 06:26:02 +0100

    Add query to list all books in a library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,14 +1,19 @@
-from relationship_app.models import Author, Book
+from relationship_app.models import Author, Book, Library, Librarian
 
-def query_books_by_author(author_name):
-    try:
-        # Get the Author object with the specified name
-        author = Author.objects.get(name=author_name)
+# Query all books by a specific author
+author_name = "Author Name"  # Replace with actual author name
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print("Books by the author:", books_by_author)
 
-        # Filter and return books written by the specific author
-        books = Book.objects.filter(author=author)
-        return books
-    except Author.DoesNotExist:
-        print("Author not found.")
-        return None
+# List all books in a library
+library_name = "Library Name"  # Replace with actual library name
+library = Library.objects.get(name=library_name)  # Fetch the library by name
+books_in_library = library.books.all()  # Get all books related to the library
+print("Books in the library:", books_in_library)
+
+# Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)  # Fetch librarian for the library
+print("Librarian of the library:", librarian)
+
 

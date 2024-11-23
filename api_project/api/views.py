ce75dc35b4f61730a8e31bb4f4e3e3ce7605a09d
Ikemeh Agatha <ikemehagatha@gmail.com>
2024-11-23 05:25:16 +0100

    Implemented CRUD operations for Book model using ModelViewSet

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets  # Correct import for viewsets
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
@@ -7,6 +7,6 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 #To Update the file by defining the BookViewSet class
-class BookViewSet(ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):  # Used viewsets.ModelViewSet here
     queryset = Book.objects.all()
     serializer_class = BookSerializer

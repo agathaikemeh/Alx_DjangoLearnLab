d44c2bac5683571cbb11f96e4d300392fdf84d49
Ikemeh Agatha <ikemehagatha@gmail.com>
2024-11-30 14:22:33 +0100

    Added template_name for LoginView and LogoutView in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from . import views  # Import the views module
 from django.urls import path
 from django.contrib.auth import views as auth_views  # Import Django's built-in authentication views
-from .views import list_books, LibraryDetailView, register, home  # Import the register view
+from .views import list_books, LibraryDetailView, register, home  # Import views including register
 
 urlpatterns = [
     # Home URL
@@ -12,8 +12,8 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for library detail
 
     # Authentication URLs
-    path('login/', views.CustomLoginView.as_view(), name='login'),  # Login URL
-    path('logout/', views.CustomLogoutView.as_view(), name='logout'),  # Logout URL
+    path('login/', views.CustomLoginView.as_view(template_name='login.html'), name='login'),  # Login URL with template
+    path('logout/', views.CustomLogoutView.as_view(template_name='logout.html'), name='logout'),  # Logout URL with template
     path('register/', views.register, name='register'),  # Register URL
 ]
 

7d0766520ca05763ebc979dd7ca95f91c8c149b0
Ikemeh Agatha <ikemehagatha@gmail.com>
2024-12-07 13:15:19 +0100

    fix(api): ensure django_filters import in views.py for filtering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django_filters import rest_framework
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.exceptions import ValidationError
